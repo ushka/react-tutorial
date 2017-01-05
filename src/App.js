5f72e7debbd8cea3dd3a6c0acd06c6125dde2a76
Uros Urosevic <urosevic1986@gmail.com>
2017-01-05 09:26:08 +0800

    add taking turns ability so both X and O can be played

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ class Board extends React.Component {
     super();
     this.state = {
       squares: Array(9).fill(null),
+      xIsNext: true,
     };
   }
   renderSquare(i) {
@@ -46,8 +47,11 @@ class Board extends React.Component {
   }
   handleClick(i) {
     const squares = this.state.squares.slice();
-    squares[i] = 'X';
-    this.setState({squares: squares});
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    });
   }
 }
 
